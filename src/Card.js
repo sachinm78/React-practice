0837c9813aa67ee6328a47bf15823c9b439545ac
sachinm78 <sachinm78@gmail.com>
2019-11-21 13:29:03 -0600

    installed prop-types and defined them in Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 // written as a function
 function Card(props) {
@@ -17,6 +18,13 @@ Card.defaultProps = {
     cardColor: "blue",
     height: 100,
     width: 100
+}
+
+// set propType
+Card.propTypes = {
+    cardColor: PropTypes.string,
+    height: PropTypes.number,
+    width: PropTypes.number
 }
 
 // written as a class
